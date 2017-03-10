52c1bc038c80478ff7ce12cd2fc82c2a6cd794a1
liuwei <liuwei9502@163.com>
2017-03-10 15:56:21 +0800

    创建项目

---
+++ app/src/main/java/com/liuwei1995/com/kugoudemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.liuwei1995.com.kugoudemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
